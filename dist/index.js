7e16980fb3eac6c1e1e23487523dbe015b9ebf42
Chetsadakon <chetsadakon@codediva.co.th>
2023-02-02 17:08:59 +0700

    my learning

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let age = 20;
+const date = new Date();
+console.log(`${date}`);
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+let age: number = 20;
+// console.log(age)
+const date = new Date();
+console.log(`${date}`)
